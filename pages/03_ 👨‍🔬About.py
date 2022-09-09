695075a2df3e71db58fa01055963515a6338171e
Sam Andersson <sam86andersson@gmail.com>
2022-09-09 23:28:23 +0200

    Update 03_ 👨‍🔬About.py

--- pages/03_ 👨‍🔬About.py
+++ pages/03_ 👨‍🔬About.py
@@ -5,15 +5,14 @@ from PIL import Image
 
 
 # load image
-img = Image.open("hair.jpg")
+img = Image.open("Images/hair.jpg")
 
 st.image(img, use_column_width=True)
 
 st.sidebar.markdown("#")
 
 st.write("""
-		 I'm just a data scientist who wishes he still had hair....
+I'm just a data scientist who wishes he still had hair....
+""")
 
-		""")
-
-st.write("[Email](election.app.boogaloo@gmail.com)")
+st.write("election.app.boogaloo@gmail.com")
